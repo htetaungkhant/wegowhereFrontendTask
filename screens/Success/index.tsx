25b9070763ad63bc676a4168cc9af37ee11587d4
Htet Aung Khant <chitkogyi19950@gmail.com>
2024-05-18 03:02:36 +0700

    Completed Success screen

--- screens/Success/index.tsx
+++ screens/Success/index.tsx
@@ -1,9 +1,31 @@
-import { Text, View } from 'react-native'
+import { AntDesign } from '@expo/vector-icons';
+import { StyleSheet, Text, View } from 'react-native';
+
+import { LinkButton } from '@/components/Button';
+import colors from '@/constants/colors';
 
 export function SuccessScreen() {
     return (
-        <View>
-            <Text>Success</Text>
+        <View style={styles.container}>
+            <AntDesign name="checkcircle" size={72} color={colors.primary} />
+            <Text style={styles.successLabel}>Success</Text>
+            <LinkButton title="Go back to card list" href="CardList" />
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        rowGap: 8,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: colors.background
+    },
+    successLabel: {
+        marginBottom: 28,
+        fontSize: 24,
+        fontWeight: '500',
+        color: "#000"
+    },  
+})
